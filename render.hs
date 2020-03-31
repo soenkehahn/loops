595510d5d119c341d5c238c2d7ab0e06617469c4
Sönke Hahn <soenkehahn@gmail.com>
2020-03-30 23:29:16 -0400

    add album render script

---
+++ render.hs
@@ -0,0 +1,19 @@
+#!/usr/bin/env stack
+{- stack script --resolver lts-15.5 -}
+
+import Development.Shake
+import Data.Foldable
+import System.FilePath
+
+songs =
+  "huhu" :
+  "leeloo" :
+  []
+
+main = do
+  unit $ cmd "rm -rf album"
+  unit $ cmd "mkdir album"
+  forM_ (zip [1 ..] songs) $ \ (n, directory) -> do
+    let outputFile = "album" </> (show n ++ "_" ++ directory ++ ".wav")
+    putStrLn $ "rendering " ++ outputFile ++ "..."
+    unit $ cmd (Cwd directory) "looper ./run.sh --render" (".." </> outputFile)
