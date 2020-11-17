6c481ef31658b2f1794d39d93b7e35f4c4e0754b
Sönke Hahn <soenkehahn@gmail.com>
2020-11-16 22:44:54 -0500

    more olmfeld

--- olmfeld/src/Main.hs
+++ olmfeld/src/Main.hs
@@ -8,7 +8,19 @@ main :: IO ()
 main = printSamples song
 
 song :: Signal Double
-song = mix [fmap (* 0.3) bass, claps]
+song = mix [fmap (* 0.6) bass, claps]
+
+phaser :: Fractional a => Signal a -> Signal a
+phaser signal =
+  cutoff $
+    fmap (/ 2) $
+      speedup (fmap (\s -> 1 + s * 0.01) sine) (signal |> constant 0)
+        +++ signal
+  where
+    cutoff =
+      case end signal of
+        Infinite -> id
+        Finite end -> flip take end
 
 claps :: Signal Double
 claps =
@@ -19,7 +31,8 @@ claps =
     1 .> clap c''' :
     1 .> empty :
     1 .> clap f''' :
-    1 .> empty :
+    0.5 .> empty :
+    0.5 .> clap c''' :
     1 .> clap d''' :
     []
 
@@ -28,21 +41,21 @@ clap pitch =
   mix $
     map (\delay -> silence (delay * Time (1 / pitch)) |> r) [0 .. 3]
   where
-    r = ramp 1 0 0.2
+    r = ramp 1 0 0.01
 
 bass :: Signal Double
 bass =
-  divide
-    [ 3 ~> n d'',
-      1 ~> n a',
-      0.5 ~> n c'',
-      3.5 ~> n d'',
-      3 ~> n d'',
-      1 ~> n a',
-      0.5 ~> n c'',
-      3.5 ~> n d''
-    ]
-    4
+  phaser $
+    flip divide 4 $
+      3 ~> n d'' :
+      1 ~> n a' :
+      0.5 ~> n c'' :
+      3.5 ~> n d'' :
+      3 ~> n d'' :
+      1 ~> n a' :
+      0.5 ~> n c'' :
+      3.5 ~> n d'' :
+      []
 
 n :: Double -> Time -> Signal Double
 n pitch time =
