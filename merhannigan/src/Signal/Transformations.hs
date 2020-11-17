14ab50a4cb2d834a039a9eefccb254e658494fa2
Sönke Hahn <soenkehahn@gmail.com>
2020-11-16 21:04:11 -0500

    use library in merhannigan

--- merhannigan/src/Signal/Transformations.hs
+++
@@ -1,9 +0,0 @@
-module Signal.Transformations where
-
-import Signal
-import Signal.Core
-
-echo :: Time -> Double -> Signal Double -> Signal Double
-echo delay volume signal =
-  signal +++
-  fmap (* volume) (silence delay |> signal)
